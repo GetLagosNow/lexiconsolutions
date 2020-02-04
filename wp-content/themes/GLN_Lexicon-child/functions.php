b72c0372e6ee4e50a214734ab42a5cb84cfea15d
Alley Hector <alleyhector@motivateco.com>
2020-02-03 17:55:07 -0800

    Take away the multiple overlapping jquery versions we were loading and input in child functions file with noconflict

--- wp-content/themes/GLN_Lexicon-child/functions.php
+++ wp-content/themes/GLN_Lexicon-child/functions.php
@@ -10,4 +10,8 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+    wp_register_script( 'jquery3.4.1', 'https://code.jquery.com/jquery-3.4.1.slim.min.js' );
+    wp_add_inline_script( 'jquery3.4.1', 'var jQuery3_4_1 = $.noConflict(true);' );
+    wp_enqueue_script( 'plugin-javascript', plugins_url( 'js.js', __FILE__ ), array( 'jquery3.4.1' ) );
 }
