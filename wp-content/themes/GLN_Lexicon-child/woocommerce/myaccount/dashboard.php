8bd79c57747bf7479365dffc78a59c52f870f84a
Seve Sheldon <sevesheldon@gmail.com>
2020-02-12 06:51:49 -0800

    Adjust the text in the dashboard of My Account.

--- wp-content/themes/GLN_Lexicon-child/woocommerce/myaccount/dashboard.php
+++ wp-content/themes/GLN_Lexicon-child/woocommerce/myaccount/dashboard.php
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <p><?php
 	printf(
-		__( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' ),
+		__( 'This can say whatever we want it to.<br>This is your account dashboard.<br>From your account dashboard you can <a href="%3$s">edit your password and account details</a>.', 'woocommerce' ),
 		esc_url( wc_get_endpoint_url( 'orders' ) ),
 		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
 		esc_url( wc_get_endpoint_url( 'edit-account' ) )
